6c22b5c7bae3d4d89b8353447f103524b2a3967f
xTheGGx <xtheggx@gmail.com>
2025-10-28 22:25:23 -0600

    Correcciones menores.
    - Cambio en el tipo de dato idUsuario
    - Eliminacion de clases redundantes

---
+++ src/test/java/com/xtheggx/monedaDOS/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtheggx.monedaDOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
